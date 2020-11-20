36803e609b463d62a56d97942673a4f51c23ff24
Nsingletary <singletarybush@gmail.com>
2020-11-20 18:49:26 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+require 'pry'
+
+katz deli = []
+
+def line(line)
+    if line == []
+        puts "The line currently empty"
+    else
+        sentence = "The line is currently"
+        line.each with index do |name, index|
+            
